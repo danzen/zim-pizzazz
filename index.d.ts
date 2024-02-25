b2a3c4ee8283ba54136036857f00c7c59bd662c4
Dan Zen <door@danzen.com>
2024-02-25 14:10:05 -0500

    Increased ZIM to NPM 16.1.4 and above

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,21 @@
 
+import {zimVee, Shape, Container} from "zimjs"
+
 declare namespace zim {
 
-    export readonly var shapes:[string]
-    export function makeShape(type?:string|zimVee, color?:string|zimVee, width?:number, height?:number): zim.Shape;
-    export function listShapes(): void;
+    export var shapes:[string]
+    export function makeShape(config_or_type?:string|zimVee, color?:string|zimVee, width?:number, height?:number): Shape
+    function makeShape(config:{type?:string|zimVee, color?:string|zimVee, width?:number, height?:number }): Shape
+    export function listShapes(): void;   
 
-    export readonly var icons:[string]
-    export function makeIcon(type?:string|zimVee, color?:string|zimVee, scale?:number|zimVee, multi?:number, multiX?:number, multiY?:number, skewX?:number, skewY?:number, backing?:zim.Container): zim.Container;
+    export var icons:[string]
+    export function makeIcon(config_or_type?:string|zimVee, color?:string|zimVee, scale?:number|zimVee, multi?:number, multiX?:number, multiY?:number, skewX?:number, skewY?:number, backing?:Container): Container
+    function makeIcon(config: {type?:string|zimVee, color?:string|zimVee, scale?:number|zimVee, multi?:number, multiX?:number, multiY?:number, skewX?:number, skewY?:number, backing?:Container}): Container
     export function listIcons(): void;
 
-    export function makePattern(type?:string|zimVee, colors?:string|zimVee, cols?:number, rows?:number, spacingH?:number, spacingV?:number, interval?:number, startPaused?:boolean, backgroundColor?:string, gradient?:boolean, cache?:boolean): zim.Container;
+    export function makePattern(config_or_type?:string|zimVee, colors?:string|zimVee, cols?:number, rows?:number, spacingH?:number, spacingV?:number, interval?:number, startPaused?:boolean, backgroundColor?:string, gradient?:boolean, cache?:boolean): Container
+    function makePattern(config: {type?:string|zimVee, colors?:string|zimVee, cols?:number, rows?:number, spacingH?:number, spacingV?:number, interval?:number, startPaused?:boolean, backgroundColor?:string, gradient?:boolean, cache?:boolean}): Container
     export function listPatterns(): void;
-
-    export const shapes = pizzazz.shapes;
-    export const makeShape = pizzazz.makeShape;
-    export const listShapes = pizzazz.listShapes;
-
-    export const makeIcon = pizzazz.makeIcon;
-    export const listIcons = pizzazz.listIcons;
-
-    export const makePattern = pizzazz.makePattern;
-    export const listPatterns = pizzazz.listPatterns;
 
 }
 
